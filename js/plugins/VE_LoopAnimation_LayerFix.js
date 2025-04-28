3b8bea38c549f2c04875389bf0a6d7fa372d63f2
Mijio <91673971+Mijio@users.noreply.github.com>
2025-04-28 14:18:46 +0300

    animation fix part 2

--- js/plugins/VE_LoopAnimation_LayerFix.js
+++ js/plugins/VE_LoopAnimation_LayerFix.js
@@ -1,34 +1,76 @@
+/*:
+ * @plugindesc v1.05 — Полный фикс VE_LoopAnimation + одинаковая позиция анимаций для loop и обычных, без ошибок (battle+map)
+ * @author ChatGPT
+ *
+ * @help
+ * Этот плагин исправляет:
+ *  - Моргание в VE_LoopAnimation
+ *  - Одинаковую позицию анимаций (startAnimation и loopAnimation)
+ *  - Только зацикленные анимации зацикливаются
+ *  - Плавная работа и правильный слой
+ *  - Нет ошибки "position of undefined"
+ *
+ * Просто установите ниже VE_LoopAnimation.js
+ */
+
 (function() {
 
-  const _Sprite_Animation_updateMain = Sprite_Animation.prototype.updateMain;
-  Sprite_Animation.prototype.updateMain = function() {
-    if (this._animation && this._duration <= 0) {
-      if (this._loop) {
-        // Перезапускаем без удаления и без всплытия в слоях
-        this._duration = this._animation.frames.length * 4;
-        this._frameIndex = 0;
-      } else {
-        this.onEnd();
-      }
-    } else {
-      _Sprite_Animation_updateMain.call(this);
+  function Sprite_LoopedAnimation() {
+    Sprite_Animation.call(this);
+  }
+
+  Sprite_LoopedAnimation.prototype = Object.create(Sprite_Animation.prototype);
+  Sprite_LoopedAnimation.prototype.constructor = Sprite_LoopedAnimation;
+
+  Sprite_LoopedAnimation.prototype.updateMain = function() {
+    if (!this._animation) return;
+    this._duration--;
+    while (this._duration > 0 && this.isReady()) {
+      this.updateFrame();
+      break;
+    }
+    if (this._duration <= 0) {
+      this._duration = this._animation.frames.length * 4;
+      this._frameIndex = 0;
     }
   };
 
-  // Переопределяем refreshLoopAnimation
-  Sprite_Base.prototype.refreshLoopAnimation = function(data, type, index) {
-    console.log('▶ refreshLoopAnimation called for type:', type, 'with data:', data);
+  Sprite_LoopedAnimation.prototype.update = function() {
+    Sprite.prototype.update.call(this);
+    this.updateMain();
+    if (this._animation) {
+      this.updatePosition(); // Вызываем только если _animation задана
+    }
+  };
 
-    this._loopAnimSprites[type] = this._loopAnimSprites[type] || [];
-    var sprite = new Sprite_Animation();
+  // --- Только обычные анимации (startAnimation) ---
+  Sprite_Base.prototype.startAnimation = function(animation, mirror, delay) {
+    if (!animation) return;
+
+    const sprite = new Sprite_Animation();
+    sprite.setup(this._effectTarget || this, animation, mirror, delay);
+    sprite.visible = this.isSpriteVisible();
+
+    if (!this._animationSprites) this._animationSprites = [];
+    this._animationSprites.push(sprite);
+
+    if (this._effectTarget && this._effectTarget.addChild) {
+      this._effectTarget.addChild(sprite);
+    } else {
+      this.addChild(sprite);
+    }
+  };
+
+  // --- Только зацикленные анимации через нотетеги ---
+  Sprite_Base.prototype.refreshLoopAnimation = function(data, type, index) {
+    this._loopAnimSprites = this._loopAnimSprites || [];
+    var sprite = new Sprite_LoopedAnimation();
     var mirror = this.isMirrorAnimation();
     var animation = $dataAnimations[data.id];
 
-    console.log('▶ Setting up animation:', animation && animation.name);
-
     sprite.setup(this._effectTarget || this, animation, mirror, 0);
     sprite.visible = this.isSpriteVisible();
-    sprite._loop = true; // ручной флаг для зацикливания
+    this._loopAnimSprites[type] = this._loopAnimSprites[type] || [];
     this._loopAnimSprites[type].push(sprite);
 
     if (this._effectTarget && this._effectTarget.addChild) {
