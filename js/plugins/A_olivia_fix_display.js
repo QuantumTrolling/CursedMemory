dd91e3b8221b64a259b07b5959d81848f7875178
Mij <mijijodevour@gmail.com>
2026-07-09 18:57:51 +0300

    о боже что с серафимом

--- js/plugins/A_olivia_fix_display.js
+++ js/plugins/A_olivia_fix_display.js
@@ -9,6 +9,7 @@
  * - Прикрепляет окно слабостей к спрайту врага (решает перекрытие посторонними спрайтами).
  * - Задаёт Z врага пропорционально его Y: чем ниже враг на экране, тем выше его Z,
  *   и его HUD не перекрывается верхними врагами.
+ * - Предотвращает мерцание за счёт однократного добавления окна.
  *
  * Place this plugin directly below Olivia_OctoBattle.js.
  */
@@ -17,39 +18,43 @@ var Imported = Imported || {};
 
 if (Imported.Olivia_OctoBattle && Olivia.OctoBattle.WeaknessDisplay && Olivia.OctoBattle.WeaknessDisplay.Enabled) {
 
-    // Переопределяем создание окна слабостей – теперь оно будет дочерним элементом спрайта врага
+    // Однократно делаем окно слабостей дочерним элементом спрайта врага
     var _Sprite_Enemy_createWeaknessDisplayWindow = Sprite_Enemy.prototype.createWeaknessDisplayWindow;
     Sprite_Enemy.prototype.createWeaknessDisplayWindow = function() {
         _Sprite_Enemy_createWeaknessDisplayWindow.call(this);
-        if (this._weaknessWindow) {
+        if (this._weaknessWindow && !this._weaknessDisplayAdded) {
+            // Удаляем окно из предыдущего родителя (если был)
             if (this._weaknessWindow.parent) {
                 this._weaknessWindow.parent.removeChild(this._weaknessWindow);
             }
+            // Добавляем как дочерний элемент спрайта
             this.addChild(this._weaknessWindow);
-            this._weaknessWindow.x = this._weaknessWindow._factorX;
-            this._weaknessWindow.y = this._weaknessWindow._factorY;
-            this._weaknessWindow._added = true;
+            // Запоминаем, что уже добавили
+            this._weaknessDisplayAdded = true;
+            // Устанавливаем начальную позицию
+            this._weaknessWindow.x = this._weaknessWindow._factorX || 0;
+            this._weaknessWindow.y = this._weaknessWindow._factorY || 0;
         }
     };
 
-    // Корректируем обновление позиции окна для работы в локальных координатах спрайта
+    // Обновление позиции окна – теперь всегда работает в локальных координатах спрайта
     var _Window_WeaknessDisplay_updatePosition = Window_WeaknessDisplay.prototype.updatePosition;
     Window_WeaknessDisplay.prototype.updatePosition = function() {
         if (this.parent && this.parent instanceof Sprite_Enemy) {
-            this.x = this._factorX;
-            this.y = this._factorY;
+            this.x = this._factorX || 0;
+            this.y = this._factorY || 0;
         } else {
             _Window_WeaknessDisplay_updatePosition.call(this);
         }
     };
 
-    // Убираем добавление окна в _baseSprite внутри Spriteset_Battle
+    // Убираем попытки добавить окно в _baseSprite, оставляем только рефреш
     var _Spriteset_Battle_updateEnemyWeaknessWindows = Spriteset_Battle.prototype.updateEnemyWeaknessWindows;
     Spriteset_Battle.prototype.updateEnemyWeaknessWindows = function() {
         if ($gameTemp._needRefreshAllEnemyWeaknessWindows === true) {
             for (var i = 0; i < this._enemySprites.length; i++) {
                 var sprite = this._enemySprites[i];
-                if (!!sprite && !!sprite._weaknessWindow) {
+                if (sprite && sprite._weaknessWindow) {
                     sprite._weaknessWindow.refresh();
                 }
             }
@@ -57,15 +62,16 @@ if (Imported.Olivia_OctoBattle && Olivia.OctoBattle.WeaknessDisplay && Olivia.Oc
         }
     };
 
-    // ----- ДОБАВЛЯЕМ СОРТИРОВКУ ПО ГЛУБИНЕ -----
-    // Каждый кадр для каждого врага задаём Z = Y (или Y * множитель)
+    // ----- СОРТИРОВКА ПО ГЛУБИНЕ + ОБНОВЛЕНИЕ ПОЗИЦИИ ОКНА -----
     var _Sprite_Enemy_update = Sprite_Enemy.prototype.update;
     Sprite_Enemy.prototype.update = function() {
         _Sprite_Enemy_update.call(this);
-        // Чем ниже враг (больше Y), тем выше Z → рисуется поверх остальных
+        // Z = Y для правильного порядка перекрытия
         this.z = this.y;
-        // Для гарантии можно использовать множитель, если Y различаются слабо:
-        // this.z = this.y * 10;
+        // Следим, чтобы окно слабостей обновляло позицию каждый кадр
+        if (this._weaknessWindow && this._weaknessDisplayAdded) {
+            this._weaknessWindow.updatePosition();
+        }
     };
 
 }
